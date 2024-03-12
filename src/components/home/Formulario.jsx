e7592721cf58b617c258ba6244eb629b2876f228
esteb <giraldoesteban044@gmail.com>
2024-03-11 22:53:35 -0500

    validacion de formulario

--- src/components/home/Formulario.jsx
+++ src/components/home/Formulario.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import adminAxios from "../../config/axios";
+import Swal from "sweetalert2";
 
 
 export const Formulario = () => {
@@ -9,6 +10,23 @@ export const Formulario = () => {
     const [agua_proveniente, setagua_proveniente] = useState([]);
     const [edad, setedad] = useState([]);
     const [personas_hogar, setpersonas_hogar] = useState([]);
+
+    const [checkedState, setCheckedState] = useState(
+        new Array(agua_hogar.length).fill(false)
+    );
+    const [usuario, setusuario] = useState({
+        propietario: 0,
+        reacciones_alergias: '',
+        nombre: '',
+        celular: '',
+        direccion: '',
+        zipcode: '',
+        agua_hogar: '',
+        Agua_provenienteId: '',
+        casado: '',
+        edad: '',
+        Personas_hogarId: '',
+    });
 
     const calidad = [0,1,2,3,4,5,6,7,8,9,10];
 
@@ -28,36 +46,84 @@ export const Formulario = () => {
         
    }, []);
 
+   const leerdatos = e => {
+
+        setusuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })       
+   }
+
+   const validarUsuario = () => {
+
+        const { propietario,                
+                reacciones_alergias,
+                nombre,
+                celular,
+                direccion,
+                zipcode,                
+                agua_hogar,
+                Agua_provenienteId,
+                casado,
+                edad,
+                Personas_hogarId,} = usuario;
+    
+        let valido = !propietario.length || !reacciones_alergias.length || !nombre.length || !celular.length ||
+                    !direccion.length || !zipcode.length || !agua_hogar.length || !Agua_provenienteId.length ||
+                    !casado.length || !edad.length || !Personas_hogarId.length
+        return valido;
+    }
+
+   const agregar = async (e) => {
+        e.preventDefault();
+        const respuesta = await adminAxios.post('/crear-usuario', usuario);    
+        
+        if(respuesta.status == 200){
+
+            Swal.fire({
+                type: 'success',
+                icon: 'success',
+                title: 'Gracias por contactarse con nosotros',                
+            })
+
+            setTimeout(() => {
+                
+                window.location.href = window.location.href;
+            }, 3000);
+
+        }
+   }
+
     return(
         <>
-            <form>   
+            <form onSubmit={agregar} >   
                 
                 <label className="form-label top">¿Eres propietario de casa?*</label>
                 <div className="form-check">
-                    <input className="form-check-input" type="radio" name="propietario" value="1" />
+                    <input className="form-check-input" type="radio" name="propietario" value="1" onChange={leerdatos} />
                     <label className="form-check-label">Si</label>                    
                 </div>  
                 <div className="form-check">
-                    <input className="form-check-input" type="radio" name="propietario" value="0" />
+                    <input className="form-check-input" type="radio" name="propietario" value="0" onChange={leerdatos}/>
                     <label className="form-check-label">No</label>
-                </div>                 
-                
-                <label className="form-label top">¿Estas casado?*</label>
-                <select className="form-select" aria-label="Default select example">
-                    
-                {
-                    casado.map( element  => (
-                        <option key={element.id} value={element.id}>{element.nombre}</option>                        
-                    ))
-                }
-                </select>
+                </div> 
 
+                <label className="form-label top">¿Estas casado?*</label>
+                <select className="form-select" aria-label="Default select example" name="casado" onChange={leerdatos}>                    
+                    {
+                        casado.map( element  => (
+                            <option key={element.id} value={element.id}>{element.nombre}</option>                        
+                        ))
+                    }
+                </select>             
+                
+                
                 <label className="form-label top">Edad*</label>
 
-                <select className="form-select" aria-label="Default select example">
+                <select className="form-select" aria-label="Default select example" name="edad" onChange={leerdatos}>
                 {
                     edad.map( element  => (
-                        <option  key={element.id} value={element.id}>{element.rango_edad}</option>                        
+                        <option  key={element.id} value={element.id} >{element.rango_edad}</option>                        
                     ))
                 }
                 </select>
@@ -74,7 +140,7 @@ export const Formulario = () => {
                 } */}
 
                 <label className="form-label top">¿Cuantas personas viven en su hogar?*</label>
-                <select className="form-select" aria-label="Default select example">
+                <select className="form-select" aria-label="Default select example" name="Personas_hogarId" onChange={leerdatos}>
                     {
                         personas_hogar.map( persona => (
                             <option  key={persona.id} value={persona.id}>{persona.cantidad}</option>  
@@ -83,7 +149,7 @@ export const Formulario = () => {
                 </select>
 
                 <label className="form-label top">¿De donde viene el agua de su hogar?*</label>
-                <select className="form-select" aria-label="Default select example">
+                <select className="form-select" aria-label="Default select example" name="Agua_provenienteId" onChange={leerdatos}>
                     {
                         agua_proveniente.map( agua => (
                             <option  key={agua.id} value={agua.id}>{agua.procedencia_agua}</option> 
@@ -94,12 +160,12 @@ export const Formulario = () => {
                 <label className="form-label top"> ¿Ha experimentado cambios en su piel como sequedad, reacciones alérgicas o irritaciones?*</label>
 
                 <div className="form-check">
-                    <input className="form-check-input" type="radio" name="reacciones_alergias" value="1" />
+                    <input className="form-check-input" type="radio" name="reacciones_alergias" value="1" onChange={leerdatos}/>
                     <label className="form-check-label">Si</label>                    
                 </div>  
 
                 <div className="form-check">
-                    <input className="form-check-input" type="radio" name="reacciones_alergias" value="0" />
+                    <input className="form-check-input" type="radio" name="reacciones_alergias" value="0" onChange={leerdatos}/>
                     <label className="form-check-label">No</label>
                 </div> 
 
@@ -107,8 +173,8 @@ export const Formulario = () => {
 
                 {
                     agua_hogar.map( agua => (
-                        <div className="form-check">
-                            <input key={agua.id} className="form-check-input" type="checkbox" value={agua.id} />
+                        <div className="form-check" >
+                            <input key={agua.id} className="form-check-input" type="checkbox" value={agua.id} onChange={leerdatos} name="agua_hogar"/>
                             <label className="form-check-label" htmlFor="flexCheckDefault">
                                 {agua.procedencia}
                             </label>
@@ -119,26 +185,26 @@ export const Formulario = () => {
 
                 <div className="mb-3 top">
                     <label className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="name" required />
+                    <input type="text" className="form-control" name="nombre" required  onChange={leerdatos}/>
                 </div>                
 
                 <div className="mb-3">
                     <label className="form-label">Celular</label>
-                    <input type="tel" className="form-control" id="phone" required/>
+                    <input type="tel" className="form-control" name="celular" required onChange={leerdatos}/>
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Dirección</label>
-                    <input type="text" className="form-control" id="address" required/>
+                    <input type="text" className="form-control" name="direccion" required onChange={leerdatos}/>
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Zipcode</label>
-                    <input type="text" className="form-control" id="zip" required/>
+                    <input type="text" className="form-control" name="zipcode" required onChange={leerdatos}/>
                 </div>                
 
                 <div className="text-right">
-                    <button type="submit" className="btn btn-primary">Enviar</button>
+                    <button type="submit" className="btn btn-primary" disabled={validarUsuario()}>Enviar</button>
                 </div>
             </form>
         </>
